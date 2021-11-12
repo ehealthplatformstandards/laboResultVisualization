a431422b5a8b6df4981375e5f170d491893b0137
Antoine Duchâteau <ad@taktik.com>
2021-11-12 13:44:30 +0100

    Improve internal API and allow for separate Div representation of DiagnosticReport

--- src/main/java/be/fgov/ehealth/fhir/narrative/gen/DiagnosticReportHtmlGenerator.java
+++ src/main/java/be/fgov/ehealth/fhir/narrative/gen/DiagnosticReportHtmlGenerator.java
@@ -15,7 +15,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 public class DiagnosticReportHtmlGenerator {
-    private INarrativeGenerator generator;
+    private final INarrativeGenerator generator;
 
     public DiagnosticReportHtmlGenerator() {
         this.generator = new CustomThymeleafNarrativeGenerator("classpath:/narratives/diagnosticreport/narrative.properties");
@@ -26,15 +26,6 @@ public class DiagnosticReportHtmlGenerator {
     }
 
     public byte[] generateHtmlRepresentation(FhirContext fhirContext, Bundle bundle, String css) {
-        final IParser parser = fhirContext.newJsonParser().setPrettyPrint(true);
-        fhirContext.setNarrativeGenerator(this.generator);
-        final String intermediate = parser
-                .setSuppressNarratives(false)
-                .encodeResourceToString(bundle);
-
-        //There must be a better way to do this
-        final Bundle reparsed = parser.setSuppressNarratives(false).parseResource(Bundle.class, intermediate);
-
         final ByteArrayOutputStream bos = new ByteArrayOutputStream();
         try(OutputStreamWriter sw = new OutputStreamWriter(bos, StandardCharsets.UTF_8)) {
             sw.append("<html><head><style>");
@@ -42,15 +33,24 @@ public class DiagnosticReportHtmlGenerator {
                     new String(IOUtils.toByteArray(Objects.requireNonNull(this.getClass().getResourceAsStream("/narratives/diagnosticreport/narrative.css"))), StandardCharsets.UTF_8)
             );
             sw.append("</style></head><body>\r\n");
-            sw.append(((DiagnosticReport) reparsed.getEntry().stream()
-                    .filter(e -> e.getResource().fhirType().equals("DiagnosticReport")).findFirst()
-                    .orElseThrow(() -> new IllegalArgumentException("Bundle must contain a DiagnosticReport"))
-                    .getResource()).getText().getDiv().toString());
+            sw.append(generateDivRepresentation(fhirContext, bundle, css));
             sw.append("</body></html>\r\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        byte[] bytes = bos.toByteArray();
-        return bytes;
+        return bos.toByteArray();
     }
+
+    public String generateDivRepresentation(FhirContext fhirContext, Bundle bundle, String css) {
+        final IParser parser = fhirContext.newJsonParser().setPrettyPrint(true).setSuppressNarratives(false);
+        fhirContext.setNarrativeGenerator(this.generator);
+        //There must be a better way to do this
+        final Bundle reparsed = parser.parseResource(Bundle.class, parser.encodeResourceToString(bundle));
+
+        return ((DiagnosticReport) reparsed.getEntry().stream()
+                    .filter(e -> e.getResource().fhirType().equals("DiagnosticReport")).findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException("Bundle must contain a DiagnosticReport"))
+                    .getResource()).getText().getDiv().toString();
+    }
+
 }
